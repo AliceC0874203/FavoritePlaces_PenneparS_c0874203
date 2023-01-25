1936c7480c1649b2ddb7f969ea8c49ca618910da
Alice <c0874203@mylambton.ca>
2023-01-24 20:17:56 -0500

    Initial Commit

---
+++ FavoritePlaces_PenneparS_c0874203/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoritePlaces_PenneparS_c0874203
+//
+//  Created by Alice’z Poy on 2023-01-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
